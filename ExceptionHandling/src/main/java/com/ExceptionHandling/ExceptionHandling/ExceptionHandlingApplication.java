619b12e1dd568dc14de7ea4ac5eb9d64254c1d7b
Chandan Kumar <rajputchandan025@gmail.com>
2025-10-02 21:26:00 -0700

    conflict files resolved

--- ExceptionHandling/src/main/java/com/ExceptionHandling/ExceptionHandling/ExceptionHandlingApplication.java
+++ ExceptionHandling/src/main/java/com/ExceptionHandling/ExceptionHandling/ExceptionHandlingApplication.java
@@ -9,7 +9,11 @@ public class ExceptionHandlingApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ExceptionHandlingApplication.class, args);
+
 System.out.println(" git local 1/1 changes");
+
+		System.out.println(" git local 1 changes");
+
 
 	}
 
